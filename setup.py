b235b567e4ba5a52ee7ff4a6d16a47bf5d2b2417
Pappan24 <panagiotispapakonstantinou15@gmail.com>
2026-03-23 15:23:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -254,6 +254,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     license="MIT",
 )
